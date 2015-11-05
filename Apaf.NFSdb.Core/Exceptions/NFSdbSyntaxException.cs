702f3c7d679872470eb7ec3ee1c99c39c45b36ff
ideoma <ideoma@gmail.com>
2015-11-06 00:01:55 +0100

    Query Language added as first draft of antlr 4 grammar.

---
+++ Apaf.NFSdb.Core/Exceptions/NFSdbSyntaxException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Apaf.NFSdb.Core.Exceptions
+{
+    public class NFSdbSyntaxException: NFSdbLockException
+    {
+        public NFSdbSyntaxException()
+        {
+        }
+
+        public NFSdbSyntaxException(string message, params object[] args)
+            : base(string.Format(message, args))
+        {
+        }
+
+        public NFSdbSyntaxException(string message, Exception inException, params object[] args)
+            : base(string.Format(message, args), inException)
+        {
+        }
+    }
+}
